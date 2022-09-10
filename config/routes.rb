0e3731342b1f61e465764c09d37f46f7e8752489
greatmomo <moritzkalt37@gmail.com>
2022-09-10 09:13:21 +0200

    fix user list button

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :users, only: [:index]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "posts#index"
-
-  get '/users', to: 'users#index', as: 'get_users'
 end
