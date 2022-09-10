6816d620a2050def616bd8a28c6a28d32fe7fe46
greatmomo <moritzkalt37@gmail.com>
2022-09-10 09:29:45 +0200

    trying to set up name as a parameter

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook github]
   
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid, name: auth.name).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.name = auth.name
       end
   end
 
