5b644aea670d66ce1703bb47a38a9384ed923acc
greatmomo <moritzkalt37@gmail.com>
2022-09-12 20:49:17 +0200

    hide provider

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :provider, :uid) }
 
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :provider, :uid, :current_password) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :provider, :uid, :password_confirmation) }
     end
 end
